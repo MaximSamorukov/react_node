3ac672f2a9d46ce25a719f5f2910ae2e788ba533
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-01-28 18:45:02 +0300

    part 4 wip

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+require('./services/passport');
 const keys = require('./config/keys');
+require('./models/Uder');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
+require('./routes/authRoutes')(app);
 
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientId,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token: ', accessToken);
-    console.log('refresh token: ', refreshToken);
-    console.log('profile: ', profile);
-    console.log('done: ', done);
-  })
-);
 
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
