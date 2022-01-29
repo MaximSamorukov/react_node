464c5aa7821c288f40727d6e80be7de700cd16ed
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-01-29 17:36:11 +0300

    fix/

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NONE_ENV === 'production') {
-  module.exports = require('./prod');
+  module.exports = require('./prod.js');
 } else {
-  module.exports = require('./dev');
+  module.exports = require('./dev.js');
 }
 
