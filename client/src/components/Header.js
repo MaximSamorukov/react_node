f4c41d30886faad7ec018df04c460af9274bb1f9
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-01-31 16:24:43 +0300

    part 7 done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,65 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    const { auth } = this.props;
+    switch (auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">
+              Login With Google
+            </a>
+          </li>
+        );
+      default:
+        return (
+          <>
+            <li>
+              <a href="">
+                Add Credits
+              </a>
+            </li>
+            <li>
+              <a href="">
+                Credits
+              </a>
+            </li>
+            <li>
+              <a href="/api/logout">
+                Logout
+              </a>
+            </li>
+          </>
+        );
+    }
+  }
   render() {
+    const { auth } = this.props;
     return (
-      <div>Header</div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Logo
+          </Link>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return ({ auth });
+}
+
+export default connect(mapStateToProps)(Header);
